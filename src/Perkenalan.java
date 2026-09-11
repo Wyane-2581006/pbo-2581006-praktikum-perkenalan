2f0045ee3c90b2b8f6437b62b2e952f7d35649e8
Wyane Christian Edgard Panjaitan <2581006@unai.edu>
2026-09-11 09:09:55 +0700

    Input nama dan NIM

---
+++ src/Perkenalan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Perkenalan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama anda: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Silahkan Input NIM anda: ");
+        int nim = scanner.nextInt();
+    }
+}
